87effa445d58744dac1923be5ac78a84a3714c73
Michael Carter <mikeaaroncarter@gmail.com>
2023-01-12 23:54:43 -0800

    Add contact form

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,144 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      contact: {
+        name: '',
+        email: '',
+        message: '',
+      },
+      token: '',
+      ready: false,
+      isReceived: false,
+    }
+  },
+  mounted() {
+    this.$http.get('http://localhost:3001/api/generateToken').then((response) => {
+      this.token = response.data
+      this.ready = true
+    })
+  },
+  methods: {
+    /**
+     * Clear the form
+     */ 
+    clearForm() {
+      for (let field in this.contact) {
+        this.contact[field] = ''
+      }
+    },
+
+    /**
+     * Handler for submit
+     */ 
+    onSubmit(evt) {
+      evt.preventDefault();
+
+      this.ready = false;
+
+      // Send form to server  
+      this.$http.post('http://localhost:3001/api/sendEmail', this.contact, { headers: {'Authorization': this.token} }).then((response) => {
+        this.clearForm();
+        this.ready = true
+        this.isReceived = true
+      }).catch((e) => {
+        console.log(e)
+        this.ready = true
+      });
+    }
+  }
+})
+</script>
+
 <template>
   <div class="content">
     <h1>Contact</h1>
     <div class="center">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfdfsfkAF3y9VIcwN_15M_2uEQUWL0h8t63lAJi5kJ2HzlcYw/viewform?embedded=true" width="640" height="947" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <div id="contact-form" class="contact-form">
+        <p class="contact-form_title">For all inquiries, please use the contact form below.</p>
+        <div class="separator"></div>
+
+        <div v-if="!ready" class="loading">Sending...</div>
+
+        <form class="form" @submit="onSubmit">
+          <input required name="name" v-model='contact.name' placeholder="Name" type="text" autocomplete="off">
+          <input required name="email" v-model="contact.email" placeholder="E-mail" type="email" autocomplete="off">
+          <textarea required name="message" v-model="contact.message" rows="6" placeholder="Message"></textarea>
+           <button class="button" :disabled="!ready">Send</button>
+        </form>
+
+        <div v-if="isReceived" class="thanks">Got it, thanks!</div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.contact-form {
+  font-family: 16px;
+  margin: 0 auto;
+  max-width: 600px;
+  width: 100%;
+}
+
+.contact-form .separator {
+  border-bottom: solid 1px #ccc;
+  margin-bottom: 15px;
+}
+
+.contact-form .form {
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+}
+
+.contact-form_title {
+  color: #333;
+  text-align: left;
+  font-size: 1.2rem;
+}
+
+.contact-form input[type="email"],
+.contact-form input[type="text"],
+.contact-form textarea {
+  border: solid 1px #e8e8e8;
+  font-family: 'Roboto', sans-serif;
+  padding: 10px 7px;
+  margin-bottom: 15px;
+  outline: none;
+}
+
+.contact-form textarea {
+  resize: none;
+}
+
+.contact-form .button {
+  background: #000;
+  border: solid 1px #000;
+  color: white;
+  cursor: pointer;
+  padding: 10px 50px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.contact-form .button:hover {
+  background: #03ac13;
+  border: solid 1px #03ac13;
+}
+
+.contact-form input[type="email"],
+.contact-form input[type="text"],
+.contact-form textarea,
+.contact-form .button {
+  font-size: 15px;
+  border-radius: 3px
+}
+
+.contact-form .thanks,
+.contact-form .loading {
+  margin:  1rem 0;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3001/api',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     },
+  //     //cors:false
+  //   },
+  // },
 })
